f1bc898804654ca5311d70efface3a47c322c54b
Pegasis12 <plsantana246@gmail.com>
2025-05-09 11:51:22 -0300

    ajustes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+// importar plugin para criar dellay de animcao com valores
+import plugin from 'tailwindcss/plugin'
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -11,6 +14,44 @@ module.exports = {
     },
     
     extend: {
+      // keyframe menu
+      keyframes:{
+        // dei um nome pra animacao
+        slideIn:{
+          // quando estiver em 0% vou ter o seguinte estilo
+          '0%':{
+            opacity: 0,
+            transform: "translateX(-20px)"
+          },
+          // quando for 100%
+          '100%':{
+            opacity: 1,
+            transform: "translateX(0)"
+          }
+        },
+
+        // keyframevideo
+        fadeIn:{
+          // quando estiver em 0% vou ter o seguinte estilo
+          '0%':{
+            opacity: 0,
+          },
+          // quando for 100%
+          '100%':{
+            opacity: 1,
+          }
+        },
+      },
+
+      // animacao
+      animation:{
+        // nome com - precisa deixar em colchetes ["nomeqquer"]: "nomedokeyframe"
+        // menu
+        ["slide-in"]: "slideIn .4s ease-in-out forwards",
+        // video
+        ["fade-in"]: "fadeIn .4s ease-in-out forwards"
+      },
+
       colors:{
         verde: {
           200: "#ACEF75",
@@ -24,6 +65,20 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  // colocando plugin aq e adicionando uma funcao que retorna varios delays automaticamente até o 12, poderia colocar manualmente mas cm a funcao consigo add mais automaticamente
+  plugins: [
+    plugin(({ addUtilities }) => {
+      function animationDelay(){
+        const delays = {};
+        for(let i = 0; i <= 12; i++){
+          delays[`.animate-${i}`] = {
+        'animation-delay': `${i * 100}ms`
+          }
+        }
+        return delays;
+      }
+
+    addUtilities(animationDelay());
+  })],
 }
 
